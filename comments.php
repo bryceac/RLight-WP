b25b00aaddb2afa19feadca40e54811128693b68
Bryce Campbell <tonyhawk2100@gmail.com>
2018-04-20 10:31:07 -0700

    try to fix comment listing.

--- comments.php
+++ comments.php
@@ -1,26 +1,14 @@
 <section id="comments"> <!-- change comment area to section -->
 
+	<?php // retrieve comments ?>
+	<ul>
 	<?php
-	// get only approved comments
-	$args = array(
-		'post_id' => XXX,
-		'status' => 'approved'
-	);
-
-	// comment query, based on Wordpress developer handbook
-	$comments_query = new WP_Comment_Query;
-	$comments = $comments_query->query( $args );
-
-	// retrieve comments, if available
-	if ( $comments ) { ?>
-		<ul>
-		<?php foreach ($comments as $comment ) { ?>
-			<li><?php echo $comment->content; ?></li>
-		<?php } ?>
-		</ul>
-	<?php } else { ?>
-			<p>No comments can be found for this article.</p>
-	<?php } ?>
+	wp_list_comment(array (
+		'max_depth' => '6',
+		'style' => 'ul'
+	)); 
+	?>
+	</ul>
 
 	<?php comment_form(); ?>
 
