a85be3c8cb3a537174e060b95f5b95310c32f5d3
Bryce Campbell <tonyhawk2100@gmail.com>
2018-04-20 14:02:58 -0700

    work on title for wordpress

--- header.php
+++ header.php
@@ -5,7 +5,8 @@
 <meta http-equiv="Content-Type" content="text/html; charset=<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <title>
-<?php wp_title(); ?> <!-- retrieve site title -->
+<?php // retrieve blog name, and, if present, include page or post name. ?>
+<?php wp_title(' - ', true, 'right'); ?> <?php bloginfo('name'); ?>
 </title>
 <meta name="description" content="{{ article.description }}"> <!-- get article description -->
 <meta name="keywords" content="{% for tag, articles in tags %}{% if loop.last %}{{ tag }}{% else %}{{ tag }}, {% endif %}{% endfor %}"> <!-- get article tags -->
